245376eac59e3761dd1ce6fcfd3b528dd1ff9bcf
Aaron Vanston <aaron@buildpass.com.au>
2026-01-09 07:23:27 +1100

    Fix lint env vars and hook rule

--- packages/ui/src/working-branch-indicator.tsx
+++ packages/ui/src/working-branch-indicator.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+/* eslint-disable react-hooks/set-state-in-effect */
+
 import { useEffect, useState } from "react"
 import { Check, ChevronDown, X } from "lucide-react"
 import { cn } from "./cn"
